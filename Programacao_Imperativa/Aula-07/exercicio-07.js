dbbd670841630fe8eb298be36d17409baa8519b8
GuilhermeJump <89156630+GuilhermeJump@users.noreply.github.com>
2021-11-03 21:02:46 -0300

    adicionada aula-07 e exercicios

---
+++ Programacao_Imperativa/Aula-07/exercicio-07.js
@@ -0,0 +1,45 @@
+
+//console.log( !true );
+
+//console.log( !false );
+
+//console.log (!!false);
+
+//console.log (!!true);
+
+//console.log(!1);
+
+//console.log (!!1);
+
+//console.log (!0);
+
+//console.log (!!0);
+
+//console.log (!!"");
+
+//let x = 5;
+//let y = 9;
+
+//console.log ((x < 10 && x!==5));
+
+//console.log ((x>9 || x===5));==
+
+//console.log (!(x===y));
+
+
+//let x = 10;
+//let y = "a";
+
+//console.log ((y==="b" || x>= 10));
+
+//let str = ""
+//let msg = "haha!"
+//let eBonito = "false"
+//console.log(!((str || msg) && eBonito))
+
+
+
+
+
+
+
